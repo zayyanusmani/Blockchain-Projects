188de188156bd96a4b97a3444f1acba1457a50be
Zayyan Usmani <zayyanusmani6@gmail.com>
2022-08-04 03:55:39 +0500

    Final commit

---
+++ sophomore/DAO-tutorial/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x5cE0c5bBbcfF7196BaaAaFaaBCfb1D57Fa2A6e79";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
---
+++ sophomore/NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xDDf5a2489dcbad5cCb165bd6FF551eBa9C11Cda6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
